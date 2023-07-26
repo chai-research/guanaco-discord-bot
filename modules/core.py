ed21e9b49b2df640482542187990d343dfbb4117
Aleksey Korshuk <ale-kor02@mail.ru>
2023-07-26 13:07:44 -0700

    initial commit

--- modules/core.py
+++ modules/core.py
@@ -13,6 +13,15 @@ def attach_core_module(bot):
         await ctx.message.delete()
         await bot.tree.sync(guild=ctx.guild)
 
+    @bot.command(name="info", guilds=[Object(id=config.GUILD_ID)])
+    async def info(ctx: ext.commands.Context):
+        await ctx.reply(
+            "You can chat with any currently deployed model and a character from the list."
+            "Here are some commands you need to know:\n"
+            "1. `/bots` — Lists all available bots to talk with.\n"
+            "2. `/chat <submission_id> <bot_name>` — Starts conversation with the bot, served by deployed submission."
+        )
+
 
 def create_bot():
     bot = ext.commands.Bot(command_prefix='/', intents=Intents.all(), aplication_id=config.APPLICATION_ID)
